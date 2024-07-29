3ef77b99b5acb3c5959b6ae3aeb39b42d1d3d6aa
SoufianKm <soufian.kamma@gmail.com>
2024-07-29 17:47:22 +0100

    Update style

--- 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
+++ 0x02-react_props/task_2/dashboard/src/Notifications/NotificationItem.js
@@ -5,7 +5,7 @@ function NotificationItem({type, html, value}) {
   return (
     <>
       {type && value ? <li data-notification-type={type}>{value}</li> : null}
-      {html ? <li data-urgent dangerouslySetInnerHTML={{ __html: html }}></li> : null}
+      {html ? <li  data-notification-type={type} dangerouslySetInnerHTML={{ __html: html }}></li> : null}
     </>
   );
 }
