4bd560c6881451ed3804061c7cde79cf487eeed6
SoufianKm <soufian.kamma@gmail.com>
2024-08-02 02:33:00 +0100

    fix bugs

--- 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications/Notifications.js
@@ -28,47 +28,31 @@ function Notifications({ displayDrawer, listNotifications }) {
             >
               <img src={close} alt="close" />
             </button>
-            <p>Here is the list of notifications</p>
-            <ul>
-              {listNotifications && listNotifications.length > 0 ? (
-                listNotifications.map(({ id, html, type, value }) => (
-                  <>
+
+            {listNotifications && listNotifications.length > 0 ? (
+              <>
+                <p>Here is the list of notifications</p>
+                <ul>
+                  {listNotifications.map(({ id, html, type, value }) => (
                     <NotificationItem
                       key={id}
                       type={type}
                       value={value}
                       html={html}
                     />
-                  </>
-                ))
-              ) : (
-                <div className="">
-                  <NotificationItem value="No new notification for now" />
-                  <button
-                    style={{
-                      color: "#3a3a3a",
-                      fontWeight: "bold",
-                      background: "none",
-                      border: "none",
-                      fontSize: "10px",
-                      position: "absolute",
-                      right: "2px",
-                      top: "2px",
-                      cursor: "pointer",
-                    }}
-                    aria-label="Close"
-                    onClick={console.log("Close button has been clicked")}
-                  >
-                    <img src={closeIcon} alt="closeIcon" width="10px" />
-                  </button>
-                </div>
-              )}
-            </ul>
+                  ))}
+                </ul>
+              </>
+            ) : (
+              <p>No new notification for now</p>
+            )}
           </div>
         </div>
       ) : (
-        <div className="menuItem">
-          <p>Your notifications</p>
+        <div className="flex-area">
+          <div className="menuItem">
+            <p>Your notifications</p>
+          </div>
         </div>
       )}
     </React.Fragment>
