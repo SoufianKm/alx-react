e12cd716730f062efdf897b5176d5e23c8956f96
SoufianKm <soufian.kamma@gmail.com>
2024-08-12 20:12:04 +0100

    Controlled components and state callback

---
+++ 0x05-react_state/task_1/dashboard/src/Login/Login.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { StyleSheet, css } from "aphrodite";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoggedIn: false,
+      email: "",
+      password: "",
+      enableSubmit: true,
+    };
+  }
+
+  handleLoginSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ isLoggedIn: true });
+  };
+
+  handleChangeEmail = (e) => {
+    this.setState({ email: e.target.value });
+  };
+
+  handleChangePassword = (e) => {
+    this.setState({ password: e.target.value });
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.email !== prevState.email ||
+      this.state.password !== prevState.password
+    ) {
+      if (this.state.email !== "" && this.state.password !== "") {
+        this.setState({ enableSubmit: true });
+      } else {
+        if (this.state.enableSubmit !== false) {
+          this.setState({ enableSubmit: false });
+        }
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className={css(styles.AppBody)}>
+        <p>Login to access the full dashboard</p>
+        <form
+          className={css(styles.AppBodyForm, styles.small)}
+          onSubmit={this.handleLoginSubmit}
+        >
+          <label htmlFor="email" className={css(styles.AppBodyLabel)}>
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            className={css(styles.AppBodyInput)}
+            value={this.state.email}
+            onChange={this.handleChangeEmail}
+          />
+          <label htmlFor="password" className={css(styles.AppBodyLabel)}>
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            className={css(styles.AppBodyInput)}
+            value={this.state.password}
+            onChange={this.handleChangePassword}
+          />
+          <input
+            type="submit"
+            className={css(styles.AppBodyButton)}
+            value="OK"
+            disabled={this.state.enableSubmit}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  login: {
+    display: "flex",
+    justifyContent: "start",
+    flexDirection: "column",
+    alignItems: "flex-start",
+  },
+  loginSection: {
+    display: "inline-flex",
+    gap: "1rem",
+    "@media (max-width:767px)": {
+      display: "grid",
+      justifyItems: "start",
+      gridTemplateColumns: "1fr 3fr",
+    },
+  },
+});
+
+export default Login;
---
+++ 0x05-react_state/task_1/dashboard/src/Notifications/NotificationItem.test.js
@@ -0,0 +1,78 @@
+import React from "react";
+import NotificationItem from "./NotificationItem";
+import { shallow } from "enzyme";
+
+describe("rendering components", () => {
+  // Suppress style injection before all tests
+  beforeAll(() => {
+    global.document = {
+      querySelector: jest.fn(() => ({
+        setAttribute: jest.fn(),
+      })),
+    };
+  });
+
+  // Resume style injection after all tests
+  afterAll(() => {
+    delete global.document;
+  });
+
+  it("renders NotificationItem component without crashing", () => {
+    const wrapper = shallow(<NotificationItem />);
+
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders correct html from type="default" value="test" props', () => {
+    const wrapper = shallow(<NotificationItem />);
+
+    wrapper.setProps({ type: "default", value: "test" });
+    expect(wrapper.html()).toEqual(
+      '<li data-notification-type="default">test</li>'
+    );
+  });
+
+  it('renders correct html from  html="<u>test</u>" props', () => {
+    const wrapper = shallow(<NotificationItem />);
+
+    wrapper.setProps({ html: "<u>test</u>" });
+    expect(wrapper.html()).toEqual('<li data-urgent="true"><u>test</u></li>');
+  });
+});
+
+describe("NotificationItem Component", () => {
+  it("should call the markAsRead function with the correct ID when clicked", () => {
+    const markAsReadSpy = jest.fn();
+    const wrapper = shallow(
+      <NotificationItem
+        id={1}
+        type="default"
+        value="Test notification"
+        markAsRead={markAsReadSpy}
+      />
+    );
+
+    // Find the list item and simulate a click
+    wrapper.find("li").simulate("click");
+
+    // Check if the spy was called with the correct ID
+    expect(markAsReadSpy).toHaveBeenCalledWith(1);
+  });
+
+  it("should call the markAsRead function with the correct ID when html prop is provided and clicked", () => {
+    const markAsReadSpy = jest.fn();
+    const wrapper = shallow(
+      <NotificationItem
+        id={2}
+        html="<strong>HTML notification</strong>"
+        markAsRead={markAsReadSpy}
+      />
+    );
+
+    // Find the list item and simulate a click
+    wrapper.find("li").simulate("click");
+
+    // Check if the spy was called with the correct ID
+    expect(markAsReadSpy).toHaveBeenCalledWith(2);
+  });
+});
