456c5c1046cf38c4369a1dd35a01c91d9c8eb37d
SoufianKm <soufian.kamma@gmail.com>
2024-08-30 15:27:25 +0100

    Connect Logout action creator to the Header

--- 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
+++ 0x09-react_redux_connectors_and_providers/task_2/dashboard/src/Header/Header.js
@@ -1,17 +1,48 @@
 import React from "react";
 import logo from "../../assets/holberton-logo.jpg";
 import { StyleSheet, css } from "aphrodite";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { logout } from "../actions/uiActionCreators";
 
-function Header() {
+function Header({ user, logout }) {
   return (
-    <>
+    <div className={css(styles.headerContainer)}>
       <img src={logo} alt="logo" className={css(styles.imgHeader)} />
       <h1 className={css(styles.level1)}>School dashboard</h1>
-    </>
+      {user.isLoggedIn && (
+        <div className={css(styles.logoutSection)}>
+          <p>
+            Welcome, {user.email} (
+            <a href="#" onClick={logout}>
+              Logout
+            </a>
+            )
+          </p>
+        </div>
+      )}
+    </div>
   );
 }
 
+Header.propTypes = {
+  user: PropTypes.shape({
+    email: PropTypes.string,
+    isLoggedIn: PropTypes.bool,
+  }),
+  logout: PropTypes.func.isRequired,
+};
+
+Header.defaultProps = {
+  user: { email: "", isLoggedIn: false },
+};
+
 const styles = StyleSheet.create({
+  headerContainer: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
   imgHeader: {
     width: "20%",
     "@media (max-width:414px)": {
@@ -31,6 +62,19 @@ const styles = StyleSheet.create({
       fontSize: "2rem",
     },
   },
+  logoutSection: {
+    textAlign: "right",
+  },
 });
 
-export default Header;
+const mapStateToProps = (state) => {
+  return {
+    user: state.get("user"), // Assuming user info is stored under "user"
+  };
+};
+
+const mapDispatchToProps = {
+  logout,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
