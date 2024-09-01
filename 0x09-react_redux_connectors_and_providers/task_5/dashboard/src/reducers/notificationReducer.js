06e90e4d9e7a0fdc7187f2f580c423d3cd667130
SoufianKm <soufian.kamma@gmail.com>
2024-09-01 15:50:10 +0100

    Connect notifications: Improve reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/reducers/notificationReducer.js
@@ -1,28 +1,23 @@
-// notificationReducer.js
-import { Map, List } from "immutable";
+import { Map } from "immutable";
 import {
+  SET_LOADING_STATE,
   FETCH_NOTIFICATIONS_SUCCESS,
-  MARK_AS_READ,
-  SET_TYPE_FILTER,
-} from "../actions/notificationActionTypes";
-import { notificationsNormalizer } from "../schema/notifications";
-import { fromJS } from "immutable"; // Ensure this import is included
+} from "../actions/uiActionTypes";
 
 const initialState = Map({
-  notifications: List(),
-  filter: "DEFAULT",
+  notifications: Map({}),
+  loading: false,
 });
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_LOADING_STATE:
+      return state.set("loading", action.isLoading);
+
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return state.set("notifications", fromJS(action.data)); // Ensure data is Immutable
-
-    case MARK_AS_READ:
-      return state.setIn(["notifications", action.index, "isRead"], true);
-
-    case SET_TYPE_FILTER:
-      return state.set("filter", action.filter);
+      return state.mergeDeep({
+        notifications: action.notifications,
+      });
 
     default:
       return state;
