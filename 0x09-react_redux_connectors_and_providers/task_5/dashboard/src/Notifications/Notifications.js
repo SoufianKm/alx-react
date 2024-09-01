42e8e9531d0487e131fbf2ca4f4c5daa074f8510
SoufianKm <soufian.kamma@gmail.com>
2024-09-01 15:52:58 +0100

    Connect notifications to the reducer

--- 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
+++ 0x09-react_redux_connectors_and_providers/task_5/dashboard/src/Notifications/Notifications.js
@@ -4,11 +4,17 @@ import NotificationItem from "./NotificationItem";
 import PropTypes from "prop-types";
 import NotificationItemShape from "./NotificationItemShape";
 import { StyleSheet, css } from "aphrodite";
+import { connect } from "react-redux";
+import { fetchNotifications } from "../../actions/notificationActionCreators";
 
 class Notifications extends PureComponent {
   constructor(props) {
     super(props);
     this.markNotificationAsRead = this.markNotificationAsRead.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.fetchNotifications();
   }
 
   markNotificationAsRead(id) {
@@ -78,9 +84,9 @@ class Notifications extends PureComponent {
 Notifications.propTypes = {
   displayDrawer: PropTypes.bool,
   listNotifications: PropTypes.arrayOf(NotificationItemShape),
-  handleHideDrawer: () => {},
-  handleDisplayDrawer: () => {},
-  markNotificationAsRead: () => {},
+  handleHideDrawer: PropTypes.func,
+  handleDisplayDrawer: PropTypes.func,
+  fetchNotifications: PropTypes.func.isRequired, // Added this line
 };
 
 Notifications.defaultProps = {
@@ -88,100 +94,14 @@ Notifications.defaultProps = {
   listNotifications: [],
   handleHideDrawer: PropTypes.func,
   handleDisplayDrawer: PropTypes.func,
-  markNotificationAsRead: () => {},
 };
 
-const opacityKeyframes = {
-  from: {
-    opacity: 0.5,
-  },
-
-  to: {
-    opacity: 1,
-  },
-};
-
-const translateKeyframes = {
-  "0%": {
-    transform: "translateY(0)",
-  },
-
-  "50%": {
-    transform: "translateY(-5px)",
-  },
-
-  "75%": {
-    transform: "translateY(5px)",
-  },
-
-  "100%": {
-    transform: "translateY(0px)",
-  },
-};
-
-const styles = StyleSheet.create({
-  menuItem: {
-    textAlign: "right",
-    cursor: "pointer",
-    background: "#fff8f8",
-    width: "fitContent",
-    float: "right",
-    ":hover": {
-      animationName: [opacityKeyframes, translateKeyframes],
-      animationDuration: "1s, 0.5s",
-      animationIterationCount: 3,
-    },
-  },
-  flexArea: {
-    position: "absolute",
-    width: "35%",
-    right: "0",
-    top: "0",
-    marginRight: "1rem",
-    "@media (max-width:900px)": {
-      width: "100%",
-      right: "0",
-      top: "0",
-      marginRight: "0.3rem",
-    },
-  },
-  Notifications: {
-    marginTop: "1rem",
-    padding: "0.5rem",
-    border: "2px dashed #e0354b",
-    "@media (max-width:900px)": {
-      padding: "0px",
-      border: "0px dashed rgb(224, 53, 75)",
-      position: "fixed",
-      width: "100% ",
-      height: "100%",
-      background: "white",
-      top: "0px",
-      left: "0px",
-      fontSize: "20px",
-    },
-  },
-  button: {
-    float: "right",
-    border: "none",
-    padding: 0,
-    background: "transparent",
-  },
-  img: {
-    width: "20px",
-  },
-  ul: {
-    "@media (max-width: 767px)": {
-      listStyle: "none",
-      padding: "0px",
-    },
-  },
-  showOff: {
-    display: "none",
-  },
-  showOn: {
-    display: "block",
-  },
+const mapStateToProps = (state) => ({
+  listNotifications: state.notifications.get("messages").toJS(), // Updated this line
 });
 
-export default Notifications;
+const mapDispatchToProps = {
+  fetchNotifications,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Notifications);
